f9852ce2e7438a506870ffd9180fe585deb7f7f2
goodbaes <95baes@gmail.com>
2020-11-22 11:10:16 +0600

    pubspec descr

--- firebaseauth/lib/firebaseauth.dart
+++ firebaseauth/lib/firebaseauth.dart
@@ -1,5 +1,6 @@
 library firebaseauth;
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
